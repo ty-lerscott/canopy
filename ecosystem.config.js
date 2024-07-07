79315a10fd9306ad11cadb2b1a795cce189f534f
Tyler Scott <ty@lerscott.com>
2024-07-07 17:29:33 -0400

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
+const tarFile = 'canopy.tar.gz';
+
 module.exports = {
     apps: [
         {
             name: 'canopy',
-            script: 'npm',
-            args: 'start', // Adjust the port if needed
+            script: `tar --xzvf ${tarFile} && rm -rf ${tarFile} && npm i && npm build && npm start`,
             instances: '2', // Or a specific number of instances
             exec_mode: 'cluster', // Enables clustering mode
             env: {
