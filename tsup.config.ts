e71690d1ebe2da2a5b7db1143692319cf939d283
Tyler Scott <ty@lerscott.com>
2024-07-10 19:42:37 -0400

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "tsup";
 
 import { config } from "dotenv";
 
+console.log("PROCESS ARGS", process.env.NODE_ENV);
+console.log("PROCESS ARGS", process.env.DISCORD_WEBHOOK_URL);
+
 const env = config({
 	path: ".env.local",
 }).parsed;
