5573c75f7de8806cd1a7d48765c2a89a72fa2487
Tyler Scott <ty@lerscott.com>
2024-07-10 20:21:49 -0400

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,12 +6,13 @@ const IS_LOCAL = process.env.NODE_ENV === "development";
 
 console.log("PROCESS ARGS", process.env.NODE_ENV);
 console.log("IS_LOCAL", IS_LOCAL);
+console.log("IS_CICD", process.env.NODE_ENV === "production");
 
 dotenv.config();
 
 console.log("PROCESS ARGS", process.env.NODE_ENV);
 console.log(
-	"MATCH",
+	"MATCHES?",
 	process.env.DISCORD_WEBHOOK_URL ===
 		"https://discord.com/api/webhooks/1260671221453033623/ZFLliap4q9pAIs-hfXbuZit3vi4PM5QfRrvXY719KS5LT2mLjYhu0Ard5zU-zG7kb_p2",
 );
@@ -20,9 +21,4 @@ export default defineConfig({
 	entry: ["./src/(api|tools)/*.ts(x)?"],
 	outDir: "./src",
 	format: "esm",
-	define: {
-		"process.env.DISCORD_WEBHOOK_URL": process.env.DISCORD_WEBHOOK_URL
-			? JSON.stringify(process.env.DISCORD_WEBHOOK_URL as string)
-			: "",
-	},
 });
