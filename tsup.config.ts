95fe0b6113cf2fbdcd26a9507edab28fbaef71a6
Tyler Scott <ty@lerscott.com>
2024-07-10 20:08:32 -0400

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,21 +7,23 @@ const IS_LOCAL = process.env.NODE_ENV === "development";
 console.log("PROCESS ARGS", process.env.NODE_ENV);
 console.log("IS_LOCAL", IS_LOCAL);
 
-const env = config(
+config(
 	IS_LOCAL
 		? {
 				path: ".env.local",
 			}
 		: undefined,
-).parsed;
+);
 
 console.log("PROCESS ARGS", process.env.NODE_ENV);
 console.log("PROCESS ARGS", process.env.DISCORD_WEBHOOK_URL);
-console.log("PROCESS ARGS", env);
 
 export default defineConfig({
 	entry: ["./src/(api|tools)/*.ts(x)?"],
 	outDir: "./src",
 	format: "esm",
-	env,
+	define: {
+		"process.env.DISCORD_WEBHOOK_URL": process.env
+			.DISCORD_WEBHOOK_URL as string,
+	},
 });
