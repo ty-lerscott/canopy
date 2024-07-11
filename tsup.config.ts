c0842a42d65055c5a5859513289287cefb13ebe0
Tyler Scott <ty@lerscott.com>
2024-07-10 22:13:59 -0400

    Add fields to discord message

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	entry: ["./src/api/**/*.ts", "./src/tools/**/*.ts"],
 	outDir: "./src",
 	format: "esm",
-	define: {
+	env: {
 		"process.env.PORT": JSON.stringify(process.env.PORT || 3000),
 		"process.env.BLUR": JSON.stringify(process.env.BLUR || 10),
 		"process.env.APP_ENV": JSON.stringify(process.env.APP_ENV || "production"),
