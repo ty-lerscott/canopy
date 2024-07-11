ad68dc53d62835977c8865d668a160b6d881875a
Tyler Scott <ty@lerscott.com>
2024-07-10 21:33:44 -0400

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 import { defineConfig } from "tsup";
 
 const env = dotenv.config().parsed;
-const IS_LOCAL =
-	env?.NODE_ENV === "development" || process.env.NODE_ENV === "development";
 
 export default defineConfig({
 	entry: ["./src/api/**/*.ts", "./src/tools/**/*.ts"],
