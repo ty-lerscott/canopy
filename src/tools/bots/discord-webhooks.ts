b1cc2c535e2147baee084e9481877c64163e5472
Tyler Scott <ty@lerscott.com>
2024-07-10 22:34:16 -0400

    Clean up console logging

--- src/tools/bots/discord-webhooks.ts
+++ src/tools/bots/discord-webhooks.ts
@@ -24,49 +24,40 @@ const sendMessage = async (message: Message) => {
 	const { url, author, fields, title, image, footer, description, level } =
 		merge<Message>(DEFAULT_MESSAGE, message);
 
-	console.log("success 1");
 	let embed = new MessageBuilder()
 		.setTimestamp()
 		.setColor(LEVEL[level as Level] as unknown as number);
-	console.log("success 2");
+
 	if (author) {
 		embed.setAuthor(author.name, author.avatar, author.url);
 	}
-	console.log("success 3");
+
 	if (title) {
 		embed = embed.setTitle(title);
 	}
-	console.log("success 4");
+
 	if (description) {
 		embed = embed.setDescription(description);
 	}
-	console.log("success 5");
+
 	if (url) {
 		// @ts-ignore
 		embed = embed.setURL(url);
 	}
-	console.log("success 6");
+
 	if (Array.isArray(fields) && fields.length) {
 		for (const field of fields) {
 			embed = embed.addField(field.name, field.value, field.isInline);
 		}
 	}
-	console.log("success 7");
 
 	if (image) {
 		embed = embed.setImage(image);
 	}
-	console.log("success 8");
+
 	if (footer) {
 		embed = embed.setFooter(footer.value, footer.image);
 	}
-	console.log("success 9");
-	console.log(
-		"FINALLY",
-		process.env.DISCORD_WEBHOOK_URL,
-		process.env.DISCORD_WEBHOOK_URL ===
-			"https://discord.com/api/webhooks/1260671221453033623/ZFLliap4q9pAIs-hfXbuZit3vi4PM5QfRrvXY719KS5LT2mLjYhu0Ard5zU-zG7kb_p2",
-	);
 
 	return hook.send(embed);
 };
