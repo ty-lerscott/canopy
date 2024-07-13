8c28f03adad2a6f079f8c46a4b75a591db565dec
Tyler Scott <ty@lerscott.com>
2024-07-12 20:18:13 -0400

    working on discord url and tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,4 @@
+import "~/dotenv.mjs";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
@@ -7,14 +8,14 @@ export default defineConfig({
 	minify: true,
 	sourcemap: true,
 	external: ["dayjs"],
-	env: {
-		BLUR: process.env.BLUR || "10",
-		PORT: process.env.PORT || "3000",
-		API_KEY: process.env.API_KEY || "",
-		APP_ENV: process.env.APP_ENV || "production",
-		NODE_ENV: process.env.NODE_ENV || "production",
-		SENTRY_URL: process.env.SENTRY_URL || "",
-		SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN || "",
-		DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL || "",
-	},
+	// env: {
+	// 	BLUR: process.env.BLUR || "10",
+	// 	PORT: process.env.PORT || "3000",
+	// 	API_KEY: process.env.API_KEY || "",
+	// 	APP_ENV: process.env.APP_ENV || "production",
+	// 	NODE_ENV: process.env.NODE_ENV || "production",
+	// 	SENTRY_URL: process.env.SENTRY_URL || "",
+	// 	SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN || "",
+	// 	DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL || "",
+	// },
 });
