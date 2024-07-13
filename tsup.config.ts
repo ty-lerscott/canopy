46ed5d6cb43541e6cc02a0e377051249471bf159
Tyler Scott <ty@lerscott.com>
2024-07-12 20:38:33 -0400

    working on discord url and tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,6 @@
 import "~/dotenv.mjs";
 import { defineConfig } from "tsup";
 
-const env = {
-	BLUR: process.env.BLUR || "10",
-	PORT: process.env.PORT || "3000",
-	API_KEY: process.env.API_KEY || "",
-	APP_ENV: process.env.APP_ENV || "production",
-	NODE_ENV: process.env.NODE_ENV || "production",
-	SENTRY_URL: process.env.SENTRY_URL || "",
-	SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN || "",
-	DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL || "",
-};
-
-console.log({ env });
-
 export default defineConfig({
 	entry: ["./src/api/**/*.ts", "./src/tools/**/*.ts", "./src/utils/**/*.ts"],
 	outDir: "./dist",
@@ -21,5 +8,14 @@ export default defineConfig({
 	minify: true,
 	sourcemap: true,
 	external: ["dayjs"],
-	env,
+	env: {
+		BLUR: process.env.BLUR || "10",
+		PORT: process.env.PORT || "3000",
+		API_KEY: process.env.API_KEY || "",
+		APP_ENV: process.env.APP_ENV || "production",
+		NODE_ENV: process.env.NODE_ENV || "production",
+		SENTRY_URL: process.env.SENTRY_URL || "",
+		SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN || "",
+		DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL || "",
+	},
 });
