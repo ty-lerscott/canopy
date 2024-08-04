ba3e663e4324c8a2d4be7ad8d2b4fd16b5a08b69
Tyler Scott <ty@lerscott.com>
2024-08-04 19:08:27 -0400

    Test deploy

--- apps/resume/vite.config.ts
+++ apps/resume/vite.config.ts
@@ -1,13 +1,20 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+// TODO: check if this module is necessary
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from "@dotenvx/dotenvx";
+
+const env = dotenv.config().parsed as Record<string, string>;
+const IS_LOCAL = env.APP_ENV === "development";
 
 export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://canopy.lerscott.local:3100",
+				target: IS_LOCAL
+					? "http://canopy.lerscott.local:3100"
+					: "https://canopy.lerscott.com",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
