ead410c3b0c565238e9a24e689ae403eff608085
Tyler Scott <ty@lerscott.com>
2024-08-11 19:08:37 -0400

    Be lazy, do crime

--- packages/config/src/pm2.config.ts
+++ packages/config/src/pm2.config.ts
@@ -3,6 +3,7 @@ import generateConfig from './utils/generate-config';
 const config = {
 	apps: [
 		generateConfig('apps/server'),
+		generateConfig('packages/config'),
 		generateConfig('apps/resume'),
 	],
 };
