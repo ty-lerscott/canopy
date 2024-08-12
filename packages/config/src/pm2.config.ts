aa83e1859becfba0b8ad433968b4d1f042bbe0a4
Tyler Scott <ty@lerscott.com>
2024-08-11 22:43:14 -0400

    automatically grab apps for pm2

--- packages/config/src/pm2.config.ts
+++ packages/config/src/pm2.config.ts
@@ -1,11 +1,12 @@
+import { resolve } from "node:path";
+import {readdirSync, statSync} from 'node:fs';
 import generateConfig from './utils/generate-config';
 
+const appsPath = resolve(process.cwd(), '..', '..', 'apps');
+const apps = readdirSync(appsPath).filter((file) => statSync(resolve(appsPath, file)).isDirectory())
+
 const config = {
-	apps: [
-		generateConfig('apps/server'),
-		generateConfig('apps/database'),
-		generateConfig('apps/resume'),
-	],
+	apps: apps.map(project => generateConfig(`apps/${project}`))
 };
 
 module.exports = config;
