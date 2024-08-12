f1241d7120541126d71bb0db501a7441f3375b32
Tyler Scott <ty@lerscott.com>
2024-08-11 21:02:20 -0400

    cleanup DB

--- apps/database/src/import.ts
+++ apps/database/src/import.ts
@@ -1,31 +1,31 @@
-// import camelCase from "camelcase-keys";
-// import educationJson from "../dist/data/Education data.json";
-// import experiencesJson from "../dist/data/Experiences (1).json";
-// import resumesJson from "../dist/data/Resumes (1).json";
-// import skillsJson from "../dist/data/Skills data.json";
-// import socialsJson from "../dist/data/Socials data.json";
-// import usersJson from "../dist/data/Users data.json";
-// import client from "./client";
-// import { tables } from "./schema";
-//
-// const imports = [
-// 	{ json: usersJson, table: tables.users },
-// 	{ json: educationJson, table: tables.education },
-// 	{ json: socialsJson, table: tables.socials },
-// 	{ json: resumesJson, table: tables.resumes },
-// 	{ json: skillsJson, table: tables.skills },
-// 	{ json: experiencesJson, table: tables.experiences },
-// ];
-//
-// const importDb = async () => {
-// 	for (const thing of imports) {
-// 		await client
-// 			.insert(thing.table)
-// 			.values(camelCase(thing.json as Record<string, unknown>[]))
-// 			.run();
-// 	}
-// };
-//
-// (async () => {
-// 	await importDb();
-// })();
+import camelCase from "camelcase-keys";
+import educationJson from "./dump/education.json";
+import experiencesJson from "./dump/experiences.json";
+import resumesJson from "./dump/resumes.json";
+import skillsJson from "./dump/skills.json";
+import socialsJson from "./dump/socials.json";
+import usersJson from "./dump/users.json";
+import client from "./client";
+import { tables } from "./schema";
+
+const imports = [
+	{ json: usersJson, table: tables.users },
+	{ json: educationJson, table: tables.education },
+	{ json: socialsJson, table: tables.socials },
+	{ json: resumesJson, table: tables.resumes },
+	{ json: skillsJson, table: tables.skills },
+	{ json: experiencesJson, table: tables.experiences },
+];
+
+const importDb = async () => {
+	for (const thing of imports) {
+		await client
+			.insert(thing.table)
+			.values(camelCase(thing.json as Record<string, unknown>[]))
+			.run();
+	}
+};
+
+(async () => {
+	await importDb();
+})();
