ed8f311badf57e0fc19b872258c9c750ae0b89db
Tyler Scott <ty@lerscott.com>
2024-08-11 20:46:08 -0400

    config and DB cleanup

--- packages/config/src/utils/get-env.ts
+++ packages/config/src/utils/get-env.ts
@@ -1,17 +1,14 @@
-import {config as envConfig} from "@dotenvx/dotenvx";
 import {readFileSync} from "node:fs";
 import {resolve} from "node:path";
 
 const getEnv = (pathname: string) => {
-    return envConfig({
-        processEnv: readFileSync(resolve(pathname, ".env"), 'utf-8').split('\n').reduce((acc, line) => {
-            const [key, value] = line.split('=');
+    return readFileSync(resolve(pathname, ".env"), 'utf-8').split('\n').reduce((acc, line) => {
+        const [key, value] = line.split('=');
 
-            acc[key] = value;
+        acc[key] = value;
 
-            return acc;
-        }, {} as Record<string, string>),
-    }).parsed || {}
+        return acc;
+    }, {} as Record<string, string>)
 }
 
 export default getEnv;
